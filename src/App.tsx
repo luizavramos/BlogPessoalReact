37be65c709703c26ace868f008d3df5058891bec
luizavramos <luizavramos30@gmail.com>
2022-09-06 18:28:44 -0300

    CRUD Postagem e temas

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import Login from './paginas/Login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import ListaTema from './components/temas/listatema/ListaTema'
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem'
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
@@ -24,8 +29,12 @@ function App() {
           <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
           <Route path="/temas" element={<ListaTema />} />
           <Route path="/posts" element={<ListaPostagem />} />
-
-     
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />     
         
         </Routes>
       </div>
